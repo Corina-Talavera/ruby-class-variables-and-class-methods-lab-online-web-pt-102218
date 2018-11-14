fa783588d30074ed993ce02c726b9b7dd02d019f
Corina Talavera <corina.talavera@gmail.com>
2018-11-14 23:43:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,6 @@
 class Song 
-attr_accessor :name, :artist, :genre
-@@count = 0
-  def initialize
-    @@count += 1
-  end
-  def self.count
+  @@count = 0
+  
+  def self.count 
     @@count
-  end  
-@@artist
-  def self.artist
-    @@artist
-  end  
-@@genre
-  def self.genre
-    @@genre
-  end
-end  
+  end 
