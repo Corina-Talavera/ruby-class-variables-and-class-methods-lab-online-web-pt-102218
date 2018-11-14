be09a6ffbe3a08ff1d58b3217c73cdd5e60b067b
Corina Talavera <corina.talavera@gmail.com>
2018-11-14 23:58:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,12 +21,13 @@ class Song
         genre_count[genre] = 1
       end
     end
+    genre_count
   end
   
   @@artists = []
   
   def self.artist_count
-    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+    @@artists
   end
   
   def self.artists
