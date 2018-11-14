4040e09046770384fcfeb2f76e4213b968159b3f
Corina Talavera <corina.talavera@gmail.com>
2018-11-14 23:35:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,13 @@
 class Song 
 attr_accessor :name, :artist, :genre
 @@count = 30
+  def count=(date)
+    @count = date
+  end
+ 
+  def release_date
+    @release_date
+  end
+end
 @@artist
 @@genre
