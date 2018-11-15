2781d1069f713ba8cdc6e21c644adcf468c60ad4
Corina Talavera <corina.talavera@gmail.com>
2018-11-15 00:06:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,9 +27,9 @@ class Song
   @@artists = []
   
   def self.artist_count
-    artist_hash = Hash.new
+    artist_count = Hash.new
         @@artists.each do |artist|
-      if artist_hash[artist] == 0
+      if artist_hash[artist] == nil
         artist_hash[artist] = 1
       else
         artist_hash[artist] += 1
