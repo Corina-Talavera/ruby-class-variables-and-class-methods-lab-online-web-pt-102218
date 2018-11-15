64990004e212eae8dcce0d1d90b844e8d88cb74a
Corina Talavera <corina.talavera@gmail.com>
2018-11-15 00:02:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,7 @@ class Song
   @@artists = []
   
   def self.artist_count
-    @@artists.inject(Hash.new(0)) { |total, i|+= 1 ;total}
+    @@artists.inject(Hash.new(0))
   end
   
   def self.artists
